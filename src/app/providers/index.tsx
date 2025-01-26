bb6bacbf6c47ff8a41075e07c0bd6b70c68b7ff8
Ujen Basi <ujenbasi12@gmail.com>
2025-01-26 23:13:07 +0545

    chore: fix typo

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -4,9 +4,7 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import { env } from "~/env";
 
-const NODE_ENV: "development" | "production" = "development";
-
-if (typeof window !== "undefined" && NODE_ENV !== "development") {
+if (typeof window !== "undefined") {
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "always",
